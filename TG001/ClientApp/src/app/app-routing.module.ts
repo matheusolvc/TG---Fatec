92f66bee82d18620b9a0d9c5f77c2d8396e161a8
JOSE MARCIEL BERGAMO JUNIOR <jose.bergamo@fatec.sp.gov.br>
2019-10-05 19:19:41 -0300

    Inclusão de renderização de partial no componente do Menu

--- TG001/ClientApp/src/app/app-routing.module.ts
+++ TG001/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GerenciarBoletosComponent } from './pages/gerenciar-boletos/gerenciar-boletos.component';
+import { TelaContaComponent } from './pages/tela-conta/tela-conta.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Boletos', component: GerenciarBoletosComponent },
+  { path: 'Contas', component: TelaContaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
