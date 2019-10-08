c0e719b6a3a15321d07909e706655074cf77debb
Matheus Campos <matheusolvc@gmail.com>
2019-10-08 01:01:50 -0300

    Update - Telas

--- TG001/ClientApp/src/app/app.module.ts
+++ TG001/ClientApp/src/app/app.module.ts
@@ -3,19 +3,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { GerenciarBoletosComponent } from './pages/gerenciar-boletos/gerenciar-boletos.component';
 import { TelaContaComponent } from './pages/tela-conta/tela-conta.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-import {MatTreeModule, MatIconModule, MatButtonModule} from '@angular/material';
+import { MatTreeModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { NavItemComponent } from './nav-item/nav-item.component';
 import { RouterModule } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @NgModule({
@@ -42,9 +43,10 @@ import {MatInputModule} from '@angular/material/input';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-  
+    MatMenuModule,
+
     RouterModule.forRoot([
-      { path: 'boletos', component: GerenciarBoletosComponent},
+      { path: 'boletos', component: GerenciarBoletosComponent },
       { path: 'conta', component: TelaContaComponent },
     ])
   ],
