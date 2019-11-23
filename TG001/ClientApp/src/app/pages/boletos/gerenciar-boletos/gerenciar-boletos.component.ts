59e5054508a43871380283aeba17088abb1a2d4a
JOSE MARCIEL BERGAMO JUNIOR <jose.bergamo@fatec.sp.gov.br>
2019-11-23 20:17:53 -0300

    Inclusão de tela de Gerenciamento de Boletos.
    Falta deixa mais bonitinho, mas já insere edite e remove informações da base corretamente.

--- TG001/ClientApp/src/app/pages/boletos/gerenciar-boletos/gerenciar-boletos.component.ts
+++ TG001/ClientApp/src/app/pages/boletos/gerenciar-boletos/gerenciar-boletos.component.ts
@@ -1,34 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Boleto } from '../../../Classes/Contas/Boleto';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BoletoService } from '../../../services/boletos.service';
+import { FornecedorService } from '../../../services/fornecedor.service';
 
 @Component({
-  selector: 'app-gerenciar-boletos',
-  templateUrl: './gerenciar-boletos.component.html',
-  styleUrls: ['./gerenciar-boletos.component.css']
+	selector: 'app-gerenciar-boletos',
+	templateUrl: './gerenciar-boletos.component.html',
+	styleUrls: ['./gerenciar-boletos.component.css']
 })
 export class GerenciarBoletosComponent implements OnInit {
+	boletoForm: FormGroup;
+	title: string = "Cadastro";
+	boletoID: number;
+	errorMessage: any;
+	boleto: Boleto;
 
-  constructor(httpClient: HttpClient) {
-    this.http = httpClient;
-  }
-  private model = new Boleto();
-  private http: HttpClient;
+	constructor(private _fb: FormBuilder, private _avRoute: ActivatedRoute,
+		private _boletoService: BoletoService, private _fornecedorService: FornecedorService, private _router: Router) {
+		if (this._avRoute.snapshot.params["id"]) {
+			this.boletoID = this._avRoute.snapshot.params["id"];
+		}
 
-  ngOnInit() {
-  }
+		this.boletoForm = this._fb.group(
+			{
+				ID: [''],
+				Status: ['', Validators.required],
+				FornecedorID: ['', Validators.required],
+				Fornecedor: this._fb.group({
+					ID: [''],
+					CNPJ: [''],
+					RazaoSocial: [''],
+					CodBanco: [''],
+					Agencia: [''],
+					Conta: [''],
+					Telefone: [''],
+					Email: [''],
+					Endereco: [''],
+					Numero: [''],
+					Bairro: [''],
+					Cidade: [''],
+					UF: [''],
+				}),
+				UsuarioID: [''],
+				DataEmissao: [Date, Validators.required],
+				DataVencimento: [Date, Validators.required],
+				TipoConta: ['', Validators.required],
+				ValorDocumento: ['', Validators.required],
+				Multa: [''],
+				Juros: [''],
+				ValorAPagar: [''],
+				LinhaDigitavel: ['', Validators.required],
+				DataCriacao: [''],
+				DataAlteracao: [''],
+				DataPagamento: [''],
+				Usuario: [''],
+			});
+	}
 
-  Voltar() {
-    window.history.back();
-  }
-  Incluir() {
-    this.http.post(window.location.origin + '/api/Boletos', JSON.stringify(this.model), httpOptions)
-      .subscribe((data: Boleto) => this.model = { ...data }), error => console.error(error);
-  }
-}
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json'
-  })
+	Resetar() {
+		this.ngOnInit();
+	}
 
+	ngOnInit() {
+		if (this.boletoID > 0) {
+			this.title = "Alteração";
+			this._boletoService.getBoleto(this.boletoID).subscribe((data: Boleto) => { this.boletoForm.setValue(data) });
+			console.log(this.boletoForm);
+		}
+	}
+
+	Voltar() {
+		this._router.navigate(['/contas/boletos']);
+	}
+	Confirmar() {
+		if (!this.boletoForm.valid) {
+			return;
+		}
+		if (this.boletoID > 0) {
+			if (confirm('Confirma a Alteração do Boleto' + this.boletoID + '?')) {
+				this._boletoService.updateBoleto(this.boletoID, this.boletoForm.value)
+					.subscribe((data) => {
+						alert('Boleto ' + this.boletoID + ' Alterado com sucesso!');
+						this._router.navigate(['/contas/boletos']);
+					}, error => this.errorMessage = error)
+			}
+		}
+		else {
+			this._boletoService.saveBoleto(this.boletoForm.value)
+				.subscribe((data) => {
+					alert('Boleto ' + data.ID + 'Cadastrado com sucesso!');
+					this._router.navigate(['/contas/boletos']);
+				}, error => this.errorMessage = error)
+		}
+	}
+
+	@ViewChild("IDFornecedor") idFornecedor: ElementRef
+	CarregarFornecedor() {
+		this._fornecedorService.getFornecedor(this.boletoForm.get(["FornecedorID"]).value).subscribe(data => {
+			let CNPJ = data.CNPJ;
+			let nome = data.RazaoSocial;
+			this.boletoForm.get(["Fornecedor", "CNPJ"]).patchValue(CNPJ);
+			this.boletoForm.get(["Fornecedor", "RazaoSocial"]).patchValue(nome);
+		}, error => {
+			alert("Fornecedor não encontrado.");
+			this.boletoForm.get(["FornecedorID"]).patchValue('');
+			this.boletoForm.get(["Fornecedor", "CNPJ"]).patchValue('');
+			this.boletoForm.get(["Fornecedor", "RazaoSocial"]).patchValue('');
+			this.idFornecedor.nativeElement.focus();
+		})
+	}
 }
--- TG001/ClientApp/src/app/pages/boletos/main-boletos/main-boletos.component.ts
+++ TG001/ClientApp/src/app/pages/boletos/main-boletos/main-boletos.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { BoletoService } from '../../../services/boletos.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Boleto } from '../../../Classes/Contas/Boleto';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-main-boletos',
-  templateUrl: './main-boletos.component.html',
-  styleUrls: ['./main-boletos.component.css']
+	selector: 'app-main-boletos',
+	templateUrl: './main-boletos.component.html',
+	styleUrls: ['./main-boletos.component.css']
 })
 export class MainBoletosComponent implements OnInit {
+	ngOnInit(): void {
+		this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
+	}
+	@ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+	@ViewChild(MatSort, { static: true }) sort: MatSort;
+	constructor(private _boletoService: BoletoService, private _router: Router) {
+		this.getBoletos();
+	}
+	getBoletos() {
+		this._boletoService.getListaBoletos().subscribe(data => {
+			this.boletos = this.dataToBoletoVM(data);
+			this.dataSource = new MatTableDataSource(this.boletos);
+			this.dataSource.paginator = this.paginator;
+			this.dataSource.sort = this.sort;
+			console.log(this.dataSource);
+		});
+	}
+	boletos: BoletoData[];
+	displayedColumns: string[] = ['ID', 'CNPJ', 'DataVencimento', 'Status', 'Editar', 'Remover'];
+	dataSource = new MatTableDataSource(this.boletos);
+	applyFilter(filterValue: string) {
+		this.dataSource.filter = filterValue.trim().toLowerCase();
 
-  constructor() { }
+		if (this.dataSource.paginator) {
+			this.dataSource.paginator.firstPage();
+		}
+	}
 
-  ngOnInit() {
-  }
+	Voltar() {
+		this._router.navigate(['/contas'])
+	}
 
+	Remover(id: number) {
+		if (confirm('Confirma remoção do Boleto ' + id + '?')) {
+			this.removeBoleto(id);
+		}
+	}
+	removeBoleto(id: number) {
+		this._boletoService.deleteBoleto(id).subscribe(data => {
+			alert('Boleto ' + data.ID + ' removido com sucesso!');
+			this.getBoletos();
+		}, error => alert(error));
+	}
+
+	private dataToBoletoVM(data: Boleto[]): BoletoData[] {
+		let boletoVM: BoletoVM[] = [];
+		let boletos: Boleto[] = data;
+		boletos.forEach(b => {
+			boletoVM.push(new BoletoVM(b.ID, b.Fornecedor.CNPJ, b.DataVencimento, b.Status));
+		});
+		return boletoVM;
+	}
+}
+
+class BoletoVM implements BoletoData {
+	constructor(
+		public ID: number,
+		public CNPJ?: string,
+		public DataVencimento?: Date,
+		public Status?: string
+	) { }
+}
+
+interface BoletoData {
+	ID?: number;
+	CNPJ?: string;
+	DataVencimento?: Date;
+	Status?: string;
 }
